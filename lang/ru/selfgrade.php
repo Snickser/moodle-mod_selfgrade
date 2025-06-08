87d586e025a2364346e2b9d840610519eeffd9f5
Snickser <snickser@gmail.com>
2025-06-08 08:31:42 +0300

    add

---
+++ lang/ru/selfgrade.php
@@ -0,0 +1,32 @@
+<?php
+// This file is part of Moodle - https://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <https://www.gnu.org/licenses/>.
+
+$string['answer'] = 'Правильный ответ';
+$string['allowedit'] = 'Разрешить студенту изменить свой ответ';
+$string['content'] = 'Содержимое страницы';
+$string['modulename'] = 'Самостоятельная оценка';
+$string['modulenameplural'] = 'Самостоятельные оценки';
+$string['pluginadministration'] = 'Администрирование плагина';
+$string['pluginname'] = 'Самостоятельная оценка';
+$string['selfgrade:addinstance'] = 'Добавить новое задание по самооценке';
+$string['selfgrade:view'] = 'Просмотреть задание по самооценке';
+
+$string['viewsubmissions'] = 'Отправленные студентом работы';
+$string['nosubmissions'] = 'Пока нет заявок.';
+$string['text'] = 'Текст';
+$string['grade'] = 'Оценка';
+$string['random'] = 'Оценивать чей-то случайный ответ';
+$string['random_help'] = 'Для оценки студенту будет предоставлен не его собственный ответ, а случайный ответ другого студента.';
